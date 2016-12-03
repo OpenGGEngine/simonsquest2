ca5c7ab6ef927663c72bc412941b75cc9dca5ef1
WX <rlaterson@gmail.com>
2016-12-03 10:59:14 -0500

    Battle BSIoda'sdfsada

--- Simons'sQuest2/src/simons/squest2/states/BattleState.java
+++ Simons'sQuest2/src/simons/squest2/states/BattleState.java
@@ -5,21 +5,30 @@
  */
 package simons.squest2.states;
 
+import java.io.File;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
 /**
  *
  * @author Warren
  */
 public class BattleState extends State{
-
+    
+    public boolean myturn = true;
+    final Image battlebackground = new Image(new File("C:/res/battlebackground.jpg").toURI().toString());
+    final Image simon = new Image(new File("C:/res/simon.png").toURI().toString());
+    
+    
     public BattleState(String name) {
         super(name);
+        
     }
 
     @Override
     public void render(GraphicsContext gc) {
-        
+        gc.drawImage(battlebackground, 0, 0,1300,600);
+        gc.drawImage(simon, 0,0,323,600);
     }
 
     @Override
