bfa4186e114c0c478875a0e686c3646c3965780e
WX <rlaterson@gmail.com>
2016-12-03 09:43:15 -0500

    Main

--- Simons'sQuest2/src/simons/squest2/SimonsSQuest2.java
+++ Simons'sQuest2/src/simons/squest2/SimonsSQuest2.java
@@ -6,17 +6,57 @@
 
 package simons.squest2;
 
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import simons.squest2.states.StateMachine;
+
 /**
  *
  * @author Javier
  */
-public class SimonsSQuest2 {
-
+public class SimonsSQuest2 extends Application{
+    
+    public static final int screenwidth = 1280, screenheight = 720;
+    final Canvas screen = new Canvas(screenwidth, screenheight);
+    final GraphicsContext gc = screen.getGraphicsContext2D();
+    
+    public static StateMachine s;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.initStyle(StageStyle.DECORATED);
+
+        Group root = new Group();
+        Scene scene = new Scene(root, screenwidth, screenheight);
+
+        // gc.fillRect(75,75,100,100);
+        root.getChildren().add(screen);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+        new AnimationTimer() {
+            @Override
+            public void handle(long currentNanoTime) {
+                gc.clearRect(0, 0, screenwidth, 900);
+            }
+        }.start();
+        
+    }
+    public void loop(GraphicsContext gc){
+        s.update();
+        s.render(gc);
     }
     
 }
